20976b4c9b14baca79b194c844249ceaf95805dc
Giannos-G <giannos.gavrielides@gmail.com>
2021-09-08 00:15:53 +0300

    Time profiling tool ready

---
+++ energy_profiling/time_profiling_tool.py
@@ -0,0 +1,8 @@
+import time
+import os
+
+start_time = time.time()
+
+os.system('python /home/giannos-g/Desktop/gavrielides_thesis/energy_profiling/example.py')
+
+print("--- %s seconds ---" % (time.time() - start_time))
